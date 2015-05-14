b48888f98a62960e012512e8a2e46003738b0921
Daniel Deitschel <daniel@gocosmonaut.com>
2015-05-13 17:54:52 -0700

    Node theming
    431

--- sites/all/themes/trashclick/templates/page.tpl.php
+++ sites/all/themes/trashclick/templates/page.tpl.php
@@ -73,7 +73,7 @@
  * @ingroup themeable
  */
 ?>
-<header id="navbar" data-spy="affix" data-offset-top="60" role="banner" class="<?php print $navbar_classes; ?>">
+<header id="navbar" data-spy="affix" data-offset-top="45" role="banner" class="<?php print $navbar_classes; ?>">
   <div class="container">  
     <div class="navbar-header">
   
